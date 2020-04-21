cffb6701eda0001d8881fe6081cb185f367e40cf
Hannes Geißler <h.geiler@schille.local>
2020-04-21 13:37:06 +0200

    added smooth scroll

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 var smallMe = document.getElementById('smallMe')
 var hiddenContainer = document.getElementById('hiddenContainer')
 
-smallMe.onclick = function() {
+smallMe.onclick = function () {
   hiddenContainer.style.display = 'block'
 }
 
 var spanClose = document.getElementById('close')
-spanClose.onclick = function() {
+spanClose.onclick = function () {
   hiddenContainer.style.display = 'none'
 }
 
 var footerText = document.getElementById('footerText')
 var footerButton = document.querySelector('#footerButton')
-footerButton.onmouseover = function() {
+footerButton.onmouseover = function () {
   footerText.style.fontSize = '1.1rem'
 }
-footerButton.onmouseout = function() {
+footerButton.onmouseout = function () {
   footerText.style.fontSize = '1rem'
 }
 
-window.onload = function() {
-  document.onclick = function(e) {
+window.onload = function () {
+  document.onclick = function (e) {
     if (e.target.id !== 'smallMe') {
       hiddenContainer.style.display = 'none'
     }
@@ -33,3 +33,13 @@ function toggleLinks() {
 }
 
 document.querySelector('#burgerMenu').addEventListener('click', toggleLinks)
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    })
+  })
+})
